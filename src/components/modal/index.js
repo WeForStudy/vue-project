efd84de0d724fb8c3ce66ae308db55fc9025268a
augustxzx <1753867820@qq.com>
2018-05-10 17:02:44 +0800

    vue-project

--- src/components/modal/index.js
+++
@@ -1,9 +0,0 @@
-import Modals from 'components/modal/index'
-
-const Modal={
-  install : function(Vue){
-    Vue.component('Modal',Modals)
-  }
-};
-
-export default Modal
--- src/components/modal/index.vue
+++ src/components/modal/index.vue
@@ -1,26 +1,13 @@
 <template>
-  <div class="modal--wrapper" ref="myModal">
-  
-  </div>
+    $END$
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
-export default {
-  name: 'modal',
-  // install(Vue) {
-  //   console.log(myModal)
-  // },
-  
-}
+    export default {
+        name: "index"
+    }
 </script>
-<style lang="stylus">
-.modal--wrapper {
-  position fixed
-  top 0 
-  bottom 0
-  right 0
-  left 0
-  background-color rgba(0, 0, 0, 0.8 )
-}
+
+<style scoped>
+
 </style>
